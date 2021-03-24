1cd19a56a0a34a13aae2f08a87c8b1ba142fbac8
Fival <thefivalco@gmail.com>
2021-03-24 14:07:35 -0500

    offline data

--- Develop/public/idb.js
+++ Develop/public/idb.js
@@ -40,3 +40,53 @@ function saveRecord(record) {
     // add record to your store with add method
     budgetObjectStore.add(record)
 }
+
+function uploadBudget() {
+    // open a transaction on your db
+    const transaction = db.transaction(['budget'], 'readwrite')
+
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('budget')
+
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll()
+
+    // more to come...
+
+
+// upon a successful .getAll() execution, run this function
+getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+        fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse)
+                }
+                // open one more transaction
+                const transaction = db.transaction(['budget'], 'readwrite')
+                // access the new_budget object store
+                const budgetObjectStore = transaction.objectStore('budget')
+                // clear all items in your store
+                budgetObjectStore.clear()
+
+                alert('All saved budget has been submitted!')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+}
+
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadBudget)
